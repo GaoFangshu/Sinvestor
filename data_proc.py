1e5f6c78ba72ef6cdda8a3887271987854fe16b9
Fangshu Gao <gaofangshu@foxmail.com>
2017-05-22 21:55:40 +0800

    write functions

--- data_proc.py
+++ data_proc.py
@@ -5,6 +5,18 @@ import pandas as pd
 import re
 
 
+"""Files structure:
+    - Sinvestors
+        - data
+            - IT橘子创业公司信息.txt
+            - IT橘子创投公司数据.txt
+            - IT橘子雷达公司估值.txt
+            - 格上理财投资机构数据.txt
+        - data_proc.py
+        - main_model.py
+        - README.md
+"""
+
 def unique_element(raw_column):
 # split string to elements and get dummy variables, sep = \s
     ele_list = []
@@ -52,7 +64,9 @@ def correct_date(column):
 
 def invest_days(column_date_now, column_date_past):
 # calculate time delta
-    column_date_past[column_date_past == '-'] = column_date_now[column_date_past == '-']
+    # https://stackoverflow.com/questions/36921951/truth-value-of-a-series-is-ambiguous-use-a-empty-a-bool-a-item-a-any-o
+    column_date_past[(column_date_past == '-') | (column_date_past == '0000-00-00')] = \
+        column_date_now[(column_date_past == '-') | (column_date_past == '0000-00-00')]
     column_date_past = pd.to_datetime(correct_date(column_date_past))
     column_date_now = pd.to_datetime(correct_date(column_date_now))
     return column_date_now - column_date_past
@@ -178,7 +192,7 @@ def replace_money(x):
 # def dummy_check(a, b):
 
 
-"""------------------- Companies data -------------------
+"""Companies data
 IT橘子创业公司信息：
     id：主键，与雷达合并？
     项目名：
@@ -196,10 +210,10 @@ IT橘子创业公司信息：
     TODO:是否将金额不明确的删掉
     TODO:（项目方 - 资金方交叉变量）成员姓名12、成员职务12、人物简介12、创业经历12、工作经历12、教育经历12
     注册资金：币种问题、换算问题
-    股东信息：获得数据(人)
+    股东信息：获得数据(人) TODO：空值太多，没有放
     企业类型：转dummy
     注册时间：（公司成立时间非项目成立时间），计算距项目时间
-    股东信息1、股东信息2：
+    股东信息1、股东信息2：TODO：空值太多，舍去
 
 IT橘子雷达公司估值：
     id：主键，与IT橘子项目还是公司合并？
@@ -212,6 +226,7 @@ rawdata_itjuzi = pd.read_csv('./data/IT橘子创业公司信息.txt', sep='\t', 
 data_itjuzi = rawdata_itjuzi[rawdata_itjuzi['投资机构1'] != '-']    # 19836 rows x 150 columns
 rawdata_leida = pd.read_csv('./data/IT橘子雷达公司估值.txt', sep='\t', encoding='gbk')    # 54975 rows x 4 columns
 
+# generate variables
 dummy_round = pd.get_dummies(data_itjuzi['项目名后轮次'], prefix='dummy_项目名后轮次')\
     .drop('dummy_项目名后轮次_获投状态：不明确', axis = 1)    # 19836 rows x 17 columns
 dummy_class_first = pd.get_dummies(data_itjuzi['一级分类'], prefix='dummy_一级分类')    # 19836 rows x 18 columns
@@ -228,32 +243,38 @@ invested_amount = get_nth_investment(1, '获投金额', 15, data_itjuzi).apply(r
 dummy_round =  pd.get_dummies(data_itjuzi['获投轮次2'], prefix='dummy_获投轮次2')\
     .drop('dummy_获投轮次2_不明确', axis = 1)    # 19836 rows x 17 columns
 regi_money = data_itjuzi['注册资金'].apply(replace_money)
+dummy_company_type = pd.get_dummies(data_itjuzi['企业类型'], prefix='dummy_企业类型')\
+    .drop(['dummy_企业类型_-', 'dummy_企业类型_未公开'], axis=1)    # 19836 rows x 84 columns
+year_from_inv = invest_days(get_nth_investment(1, '获投时间', 15, data_itjuzi),
+                            (data_itjuzi['注册时间'] + '')) / np.timedelta64(365, 'D')    # years from company's establishment
 
 
-# IT橘子创投公司数据：
-#     id：主键
-#     机构简称：与格上理财数据匹配
-#     管理资本规模：解决币种问题、“其中包含”问题、资本规模币种分割问题
-#     单个投资项目规模：币种问题、换算问题
-#     投资领域：空格分割
-#     投资轮次：空格分割
-#     （项目方 - 资金方 交叉变量）投资组合时间、投资组合名称、投资组合行业、投资组合轮次、投资组合金额：空格分割、重新做表、币种问题、换算问题
-#     （项目方 - 资金方 交叉变量）投资人姓名60、投资人职位60、投资人简介60、投资人投资项目60、投资人工作经历60、投资人教育经历60：把60人以这五类汇总
-#     已投资行业、已投资各行业数量、已投资轮次、已投资各轮次数量：总表dummy然后每个投资机构填入统计
-#
-# 格上理财投资机构数据:
-#     id：？
-#     机构简称：与IT橘子数据匹配
-#     机构类型：查空值、转dummy
-#     资本类型：查空值、转dummy
-#     成立日期：计算距项目时间（是否筛选？）
-#     （项目方 - 资金方交叉变量）机构总部：查空值、转dummy
-#     管理规模：币种问题、换算问题
-#     基金个数：查空值
-#     投资数量：查空值
-#     退出数量：查空值、（退出/投资 比？）
-#     投资项目名称、投资项目行业分类、投资项目投资阶段、投资项目投资资金、投资项目投资时间：行业转转dummy，各变量统计
-#     退出项目企业名称、退出项目行业分类、退出项目退出方式、退出项目账面回报、退出项目推出时间：行业转转dummy，各变量统计
+"""Investors data
+IT橘子创投公司数据：
+    id：主键
+    机构简称：与格上理财数据匹配
+    管理资本规模：解决币种问题、“其中包含”问题、资本规模币种分割问题
+    单个投资项目规模：币种问题、换算问题
+    投资领域：空格分割
+    投资轮次：空格分割
+    （项目方 - 资金方 交叉变量）投资组合时间、投资组合名称、投资组合行业、投资组合轮次、投资组合金额：空格分割、重新做表、币种问题、换算问题
+    （项目方 - 资金方 交叉变量）投资人姓名60、投资人职位60、投资人简介60、投资人投资项目60、投资人工作经历60、投资人教育经历60：把60人以这五类汇总
+    已投资行业、已投资各行业数量、已投资轮次、已投资各轮次数量：总表dummy然后每个投资机构填入统计
+
+格上理财投资机构数据:
+    id：？
+    机构简称：与IT橘子数据匹配
+    机构类型：查空值、转dummy
+    资本类型：查空值、转dummy
+    成立日期：计算距项目时间（是否筛选？）
+    （项目方 - 资金方交叉变量）机构总部：查空值、转dummy
+    管理规模：币种问题、换算问题
+    基金个数：查空值
+    投资数量：查空值
+    退出数量：查空值、（退出/投资 比？）
+    投资项目名称、投资项目行业分类、投资项目投资阶段、投资项目投资资金、投资项目投资时间：行业转转dummy，各变量统计
+    退出项目企业名称、退出项目行业分类、退出项目退出方式、退出项目账面回报、退出项目推出时间：行业转转dummy，各变量统计
+"""
 
 rawdata_invjuzi = pd.read_csv('./data/IT橘子创投公司数据.txt', sep='\t', encoding='gbk')    # 6607 rows x 375 columns
 rawdata_geshang = pd.read_csv('./data/格上理财投资机构数据.txt', sep='\t', encoding='gbk')    # 10106 rows x 20 columns
