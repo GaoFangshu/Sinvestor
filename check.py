2384def8d57e0b0aa73880b34fe20cdfd3eda0d3
FreeA7 <987320967@qq.com>
2017-08-28 11:21:39 +0800

    multiprocessing save data, debugging

--- check.py
+++ check.py
@@ -3,6 +3,7 @@ from random import randint
 import numpy as np
 import data_proc
 import pandas as pd
+import multiprocessing
 
 def paste_string(data, name, num_start, num_end):
     string = ''
@@ -59,7 +60,7 @@ class Observation:
         self.investor_now = data_proc.get_nth_investment(1, '投资机构', 15, self.data_itjuzi)
         self.investor_name_list = self.data_invjuzi['投资机构名称'].loc[self.data_investors.index]
 
-    def gen_observation(self):
+    def gen_observation(self, save_flag=0, queue=0):
         # TODO: Multithread?
         def check(loc_company, loc_investor):
             """For random location (row), calculate variables of interaction."""
@@ -105,17 +106,42 @@ class Observation:
                          np.array([0])]
                 # print('Sample prepared (0)')
                 return sample
-        while 1:
-            random_company = [randint(0, self.train_size - 1) for i in range(self.batch_size_main)]
-            index_random_company = self.data_companies.index[random_company]
-            random_invest = [randint(0, len(self.data_investors) - 1) for i in range(self.batch_size_main)]
-            index_random_invest = self.data_investors.index[random_invest]
-            # print(random_invest)
-            sample = [get_sample(index_random_company[i], index_random_invest[i]) for i in range(self.batch_size_main)]
-            yield ({'input_company':np.asarray([i[0] for i in sample]), 'input_investor':np.asarray([i[1] for i in sample]),
-                    'input_cross':np.asarray([i[2] for i in sample])}, {'output_dense':np.asarray([i[3] for i in sample])})
-##            yield (np.asarray([get_sample(index_random_company[i], index_random_invest[i])[0] for i in range(self.batch_size_main)]),
-##                 np.asarray([get_sample(index_random_company[i], index_random_invest[i])[1] for i in range(self.batch_size_main)]))
+            
+        if save_flag == 1:
+            while 1:
+                random_company = [randint(0, self.train_size - 1) for i in range(self.batch_size_main)]
+                index_random_company = self.data_companies.index[random_company]
+                random_invest = [randint(0, len(self.data_investors) - 1) for i in range(self.batch_size_main)]
+                index_random_invest = self.data_investors.index[random_invest]
+                # print(random_invest)
+                sample = [get_sample(index_random_company[i], index_random_invest[i]) for i in range(self.batch_size_main)]
+                queue.put(pd.DataFrame([np.concatenate([i[0], i[1], i[2], i[3]]) for i in sample]))
+
+        else:
+            while 1:
+                random_company = [randint(0, self.train_size - 1) for i in range(self.batch_size_main)]
+                index_random_company = self.data_companies.index[random_company]
+                random_invest = [randint(0, len(self.data_investors) - 1) for i in range(self.batch_size_main)]
+                index_random_invest = self.data_investors.index[random_invest]
+                # print(random_invest)
+                sample = [get_sample(index_random_company[i], index_random_invest[i]) for i in range(self.batch_size_main)]
+                yield ({'input_company':np.asarray([i[0] for i in sample]), 'input_investor':np.asarray([i[1] for i in sample]),
+                        'input_cross':np.asarray([i[2] for i in sample])}, {'output_dense':np.asarray([i[3] for i in sample])})
+
+
+def writeinworker(queue, path='./data/saved_data/'):
+    while 1:
+        if not queue.empty():
+            data = queue.get(True)
+            data.to_csv('%ssaved_data.csv'%path, index=0)
+        else:
+            continue
+    
+
+
+WORKER_NUM = 10
+
+
 
 if __name__ == '__main__':
     data_companies = data_proc.DataCompany()
@@ -123,6 +149,7 @@ if __name__ == '__main__':
     data_companies.import_data_radar()
     data_companies.gen_variables()
     data_companies.gen_data()    # Output: data_companies.data
+    print('■■■■■■■ 已读取 初创公司数据，维度：(%d, %d) ■■■■■■■'%(data_companies.data.shape[0], data_companies.data.shape[1]))
 
     data_investors = data_proc.DataInvestor()
     data_investors.import_data_invjuzi()
@@ -135,13 +162,51 @@ if __name__ == '__main__':
                            right_index=True)
     data_investors.delete_no_recent(data=merged_data, column=1)
     data_investors.gen_model_data()    # Output: data_investors.model_data
+    print('■■■■■■■ 已读取 投资机构数据，维度：(%d, %d) ■■■■■■■'%(data_investors.model_data.shape[0], data_investors.model_data.shape[1]))
 
     observation = Observation(data_companies.data,
                               data_investors.model_data,
                               data_companies.data_itjuzi,
                               data_investors.data_invjuzi,
                               train_size=3000,
-                              batch_size_main=1000)
-    observation.gen_observation()
+                              batch_size_main=30)
+    print('■■■■■■■ 训练数据准备生成 ■■■■■■■')
+
+    multiprocessing.freeze_support()
+    pool = multiprocessing.Pool()
+    manager = multiprocessing.Manager()
+
+    queue_data = manager.Queue()
+    queue_writein = manager.Queue()
+
+    print('■■■■■■■ 多进程准备完毕 ■■■■■■■')
+
+    for worker in range(WORKER_NUM):
+        pool.apply_async(observation.gen_observation, args=(1, queue_data))
+    pool.close()
+
+    print('■■■■■■■ 工作进程准备完毕 ■■■■■■■')
+
+    writein = multiprocessing.Process(target=writeinworker, args=(queue_writein,))
+    writein.start()
+
+    print('■■■■■■■ 写入进程准备完毕 ■■■■■■■')
+    print('■■■■■■■ 开始捕获生成数据 ■■■■■■■')
+
+    savedata = pd.DataFrame()
+    count = 0
+    while 1:
+        if not queue_data.empty():
+            data_frame = queue_data.get(True)
+            savedata = pd.concat([savedata, data_frame])
+            count += 1
+            print('-- %d rows get it'%(count * 30))
+            if count % 100 == 0:
+                queue_writein.put(savedata)
+                print('---- %d rows writein'%(count * 30))
+        else:
+            continue
+            
+        
 
 # --------------------- End ----------------------
